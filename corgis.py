e032dba1d73c13c185758ba1ba80349d784c33fb
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-04-08 10:20:59 -0700

    Update corgis.py

--- corgis.py
+++ corgis.py
@@ -6,6 +6,38 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+@app.route('/funFact')  
+def render_fun_fact():
+    state_chosen = request.args['state']
+    return render_template('random.html', options=get_state_options(), funFact=fun_fact_by_state(state_chosen))
+    
+def get_state_options():
+    listOfStates = [] #makes an empty list
+    with open('county_demographics.json') as county_demographics_data:
+        counties = json.load(county_demographics_data)
+    for county in counties:
+        if not(county["State"] in listOfStates):
+            listOfStates.append(county["State"])
+        
+    options = ""
+    for state in listOfStates:
+        options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
+    return options
+    
+def fun_fact_by_state(state):
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    first_county = "ZZZZZZZ"
+    for county in counties:
+        if county["County"] < first_county and county["State"] == state:
+            first_county = county["County"]
+    return "In alphabetical order " + first_county + " comes up first!"
+
+
+
+
+
+
     
 if __name__=="__main__":
     app.run(debug=False)
